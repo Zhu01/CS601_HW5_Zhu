599f521e872e6885f737161458fc3dc88ba5ee67
Zhu01 <43347096+Zhu01@users.noreply.github.com>
2023-06-13 15:56:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,48 @@
-document.getElementById("button").onclick = function () {
-    // fetch json data
-    fetch("https://zhu01.github.io//degrees.json")
-        .then(response => response.json())
-        .then(data => {
-            // create the table and attach to fetchedData div
-            const table = createTable(data.degrees);
-            document.getElementById("fetchedData").appendChild(table);
-        })
-        .catch(error => {
-            console.error("Error:", error);
+document.addEventListener("DOMContentLoaded", function (event) {
+  let button = document.getElementById("btn");
+
+  //Add event listener for the button when button be clicked
+  button.addEventListener("click", function (event) {
+    //Clear everything when clicking on the button
+    let div = document.getElementById("container");
+    div.innerHTML = "";
+
+    fetch("https://zhu01.github.io/degrees.json").then((response) => {
+      if (response.status == 200) {
+        response.json().then((data) => {
+          //Create a table
+          const table = document.createElement("table");
+
+          //Get and creat the header for table
+          let headers = [];
+          for (let item in data.my_degrees[0]) {
+            headers.push(item);
+          }
+
+          let tr = table.insertRow(-1);
+          for (let i = 0; i < headers.length; i++) {
+            let th = document.createElement("th");
+            th.innerHTML = headers[i];
+            tr.appendChild(th);
+          }
+
+          //Create other table rows with degrees information
+          for (let i = 0; i < data.my_degrees.length; i++) {
+            let tr = table.insertRow(-1);
+            let degree = data.my_degrees[i];
+            for (let header of headers) {
+              let td = document.createElement("td");
+              td.innerHTML = degree[header];
+              tr.appendChild(td);
+            }
+          }
+
+          //Add the whole table to the page
+          div.appendChild(table);
         });
-};
-
-// function to create the table dynamically
-function createTable(data) {
-    const table = document.createElement("table");
-    const headers = Object.keys(data[0]); 
-
-    // creates the table header row
-    const headerRow = table.insertRow();
-    headers.forEach(header => {
-        const th = document.createElement("th");
-        th.textContent = header;
-        headerRow.appendChild(th);
+      } else {
+        div.innerHTML = "Sorry, there may some error!";
+      }
     });
-
-    // creates rows for each data set
-    data.forEach(degree => {
-        const row = table.insertRow();
-        Object.values(degree).forEach(value => {
-            const cell = row.insertCell();
-            cell.textContent = value;
-        });
-    });
-
-    return table;
-}
+  });
+});
